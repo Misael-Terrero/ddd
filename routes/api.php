7430dff2e508501bcc511ada4341dbc53ece1a0d
JuanjoRuiz <juanjose.ruiz.munoz@gmail.com>
2025-03-25 15:31:39 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-
-
-Route::prefix('platform_purchase')->group(base_path('src/platform/purchase/infrastructure/routes/api.php'));
