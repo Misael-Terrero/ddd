b20f4bea92524aa34548647865e5f262785ab5b8
Misael <misaelterrero96@gmail.com>
2025-09-27 10:56:43 -0400

    subiendo a al repo

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::prefix(prefix: 'admin/user')->group(base_path(path: 'src/admin/user/infrastructure/routes/api.php'));
